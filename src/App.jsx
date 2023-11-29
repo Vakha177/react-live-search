b3c1b05034aa085c58e418d335f18f6fb52e7fe9
Vakha Gonikeev <sheikh2003@mail.ru>
2023-11-29 15:27:23 +0300

    V

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Main from './Main/Main';
+import Goods from './Main/Goods';
+
+
+const App = () => {
+  return (
+    <div className="app">
+     <Main/>
+    
+    </div>
+  );
+};
+
+export default App;
